0d017ed0e13a5711b8f85f82c941cb9a25c54df6
Rajeev Jha <jha.rajeev@gmail.com>
2026-06-30 18:06:08 +0530

    simplified Kafka Proxy
    The Kafka message delivery is asynchronous and errors and any broker
    errors are throws inside the  KafkaPublisher Lambda handler. The errors
    do not bubble up to KafkaProxy, hence there is no point of writing
    Synchronous try-catch blocks in Kafka Proxy.

--- xapi/src/main/java/online/softmaxx/xapi/kafka/KafkaProxy.java
+++ xapi/src/main/java/online/softmaxx/xapi/kafka/KafkaProxy.java
@@ -4,7 +4,7 @@ import online.softmaxx.xapi.service.model.PhoneRecord;
 import online.softmaxx.xapi.service.otp.OtpType;
 
 public final class KafkaProxy {
-
+    
     private KafkaProxy() {
         throw new UnsupportedOperationException("KafkaProxy class cannot be instantiated");
     }
@@ -13,15 +13,9 @@ public final class KafkaProxy {
         final String token, 
         final OtpType otpType) {
 
-        try {
-            
-            final String payload = "code: " + token + ",phone:" + phoneRecord.e164Phone();
-            // Forward execution to the Kafka client
-            KafkaPublisher.send("xapi_tube", phoneRecord.e164Phone(), payload);
-            
-        } catch (final Exception ex) {
-            throw new KafkaException("unable to send event to kafka client");
-        }
+        final String payload = "code: " + token + ",phone:" + phoneRecord.e164Phone();
+        // Forward execution to the Kafka client
+        KafkaPublisher.send("xapi_tube", phoneRecord.e164Phone(), payload);
 
     }
 }
